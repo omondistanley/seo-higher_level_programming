ed975da6b86dacf5c268caf446a72507b0213504
dimmed82 <omondistanley.oduor@gmail.com>
2023-06-08 13:33:39 -0700

    part six

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,11 +3,10 @@ def print_matrix_integer(matrix=[[]]):
     rows = len(matrix)
     for i in range(rows):
         columns = len(matrix[i])
-        a = []
         for j in range(columns):
-            k = int(input())
-            a.append(k)
-        matrix.append(a)
-    for i in range(rows):
-        for j in range(columns):
-            print("{:d}{:d}".format(matrix[i][j]), sep=" ")
+            if j != columns -1:
+                separator = ' '
+            else:
+                separatoer = ''
+            print("{:d}".format(matrix[i][j]), end =separator)
+        print("")

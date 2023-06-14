fdff4f96819dd60042e295f8aa2c80f58fa0f3fe
dimmed82 <omondistanley.oduor@gmail.com>
2023-06-14 07:09:48 -0700

    part seven

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) == 0:
+        element1, element2 = (0, 0)
+    elif len(tuple_a) == 1:
+        element1 = tuple_a[0]
+        element2 =0
+    else:
+        element1 = tuple_a[0]
+        element2 = tuple_a[1]
+    if len(tuple_b) == 0:
+        element3, element4 = (0,)
+    elif len(tuple_b) = 1:
+        element3 = tuple_b[0]
+        element4 = 0
+    else:
+        element3 = tuple_b[0]
+        element4 = tuple_b[1]
+    sum1 = element1 + element3
+    sum2 = element2 + element4
+    sumTuple = (sum1, sum2)
+    print(sumTuple)

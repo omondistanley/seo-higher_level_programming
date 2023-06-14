cb08642bef82ac77a73aa505ab2832f8fc2b5d38
dimmed82 <omondistanley.oduor@gmail.com>
2023-06-14 06:28:15 -0700

    Part Twelve

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,11 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", sep=' ')
+            print("FizzBuzz", sep='')
         elif i % 3 == 0:
-            print("Buzz", sep=" ")
+            print("Buzz", sep="")
         elif i % 5 == 0:
-            print("Fizz", sep=" ")
+            print("Fizz", sep="")
         else:
-            print(i, sep=" ")
+            print(i, sep="")
             print(end=" ")

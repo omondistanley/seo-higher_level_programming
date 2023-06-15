40b3445fdca0390618f2b30986132de822d26dd6
dimmed82 <omondistanley.oduor@gmail.com>
2023-06-15 13:53:29 -0700

    Part three

---
+++ python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    A square class based on the 2-square.py with a private
+    instance attribute of size inistantiated to be an int
+    greater than 0. The class also has the private instance
+    method that returns the current area of the square.
+"""
+
+
+class Square:
+    """ The square class based on the above comments. """
+
+    def __init__(self,size=0):
+        """ Initializes the size, to size after ensuring its
+        an integer and greater than zero. """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            """ If the size meets the above conditions, then
+            the square's size is set to size. """
+            self.__size = size
+
+    def area(self):
+        area = self.__size * self__size
+        return area

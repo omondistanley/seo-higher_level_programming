1587d1feda33958979bd59ddf9f71b1d26dd41a1
dimmed82 <omondistanley.oduor@gmail.com>
2023-06-14 18:30:20 -0700

    Part one

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """
-    A square class based on the 0-square.py, with a 
+    A square class based on the 0-square.py, with a
     private instance attribute of size, the instantiation
     of size wih no type
 """
+
 
 class Square:
     """ The square class with the private instance attribute
         and the initialization of the size attribute. """
 
     def __init__(self, size):
-        """ the self is used to limit the size attribute when 
+        """ the self is used to limit the size attribute when
             initialized to the class only. """
         # the private instance attribute is assigned the size.
         self.__size = size

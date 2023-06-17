757f678585cba9e7220e9c9b753c31c1f76c3f78
dimmed82 <omondistanley.oduor@gmail.com>
2023-06-16 18:43:41 -0700

    Part six

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -37,8 +37,8 @@ class Square:
 
     def position(self, value):
         if (type(value) is tuple and len(value) == 2 and
-        type(value[0]) == int and type(value[1]) == int
-         and value[0] >= 0 and value[1] >= 0):
+            type(value[0]) == int and type(value[1]) == int
+            and value[0] >= 0 and value[1] >= 0):
                     self.position = value
         else:
             raise TypeError("position must be a tuple of two integers")

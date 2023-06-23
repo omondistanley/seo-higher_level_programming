a8eae33d1f384683cb996ff9ea29c4135ddbd842
dimmed82 <omondistanley.oduor@gmail.com>
2023-06-23 08:22:38 -0700

    Part seven

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,23 +1,38 @@
-#!/usr/bin/python3
+#!usr/bin/python3
+""" The add tuple class, adding two integers in the two
+    tuples, the tuples must have integers and if smaller
+    than 2, use 0 for each missing integer and the first
+    two if the tuple if bigger than the two tuples. """
+
+    def add_tuple(tuple_a=(), tuple_b=()):
+        # check the length of the tuples: 
+        # 1. If the length is 0 set each element to 0
+        if len(tuple_a) == 0:
+            firstElement, secondElement = (0, 0)
+        #2. if the length is 1 then set an element to the
+        #   value.
+        elif len(tuple_a) == 1:
+            firstElement = tuple_a[0]
+            secondElement = 0
+        #3. if the length is 2 or greater set the values to
+        # be the elements.
+        else:
+            firstElement = tuple_a[0]
+            secondElement = tuple_a[1]
+        # second tuple.
+        if len(tuple_b) == 2:
+            thirdElement = tuple_b[0]
+            fourthElement = tuple_b[1]
+        elif len(tuple_b0 == 1:
+            thirdElement = tuple_b[0]
+            fourthElement = 0
+        else:
+            thirdElement, fourthElement = (0, 0)
+        # add the elements in the tuples to come up with a new
+        # tuple
+        firstSum = firstElement + thirdElement
+        secondSum = secondElement + fourthElement
+        sumTuple = (firstSum, secondSum)
+        return sumTuple
 
 
-def add_tuple(tuple_a=(), tuple_b=()):
-
-    if len(tuple_a) == 0:
-        element1, element2 = (0, 0)
-    elif len(tuple_a) == 1:
-        element1 = tuple_a[0]
-        element2 = 0
-    else:
-        element1 = tuple_a[0]
-        element2 = tuple_a[1]
-    if len(tuple_b) == 0:
-        element3, element4 = (0, 0)
-    elif len(tuple_b) == 1:
-        element3 = tuple_b[0]
-        element4 = 0
-    else:
-        element3 = tuple_b[0]
-        element4 = tuple_b[1]
-    sumTuple = (element1 + element3, element2 + element4)
-    print(sumTuple)

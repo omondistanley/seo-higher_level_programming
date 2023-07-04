a4cff436f7bc272966449f3235f5454d156f7ec8
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-04 11:28:40 -0700

    Part one

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    Script listing all the states starting with letter N
+    from the database. 
+"""
+
+import MySQLdb as db
+import sys
+
+if __name__ = "__main__";
+    # the arguments, username, password and database are taken
+    # from the comand line.
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    """ Connect to a MySQL server running on a local host and
+        the given port. 
+    """
+    db = MySQLdb.connect(host = "localhost", port = 3306; user =
+            username, pin = password, db = database)
+
+    """
+        Create a cursor object connected to the database. 
+    """
+    cursor = db.cursor()
+

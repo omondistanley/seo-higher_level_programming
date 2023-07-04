94161e3a67e88609d4c4e3ac3f971462d377561b
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-04 07:34:21 -0700

    Part zero

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -12,21 +12,23 @@ from sys import argv
 
 
 if __name__ == '__main__';
-# the arguments, username, password and database name and
-# in order. 
+    # the arguments, username, password and database name and
+    # in order. 
     username = argv[0]
     password = argv[1]
     database = argv[2]
-# connect to a MySQL server running on a localhost port 3306
+    # connect to a MySQL server running on a localhost port 3306
     db = MySQLdb.connect(host = 'localhost', port = 3306,
             user = username, pwd = password, db = database)
     cur = db.cursor()
-    # select everything, from states database/file and ordered by
+    """ select everything, from states database/file and ordered by
     # state id in ascending order
-    # cursor executes the select, from and order by statements
+    # cursor executes the select, from and order by statements """
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    # gets all rows from a cursor object
+    """ gets all rows from a cursor object """
     rows = cur.fetchall()
+    """ iterates through and prints out all the rows, then close the
+    cursor and database objects. """
     for row in rows:
         print(row)
     cur.close()

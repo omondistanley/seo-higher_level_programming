a26b1d3b023187c92885119670a93fde2e3cb5cd
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-04 10:49:53 -0700

    Part zero

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,24 +5,34 @@
     password, database name. Must use the MySQLdb module and
     results must be sorted in ascending order.
 """
-if __name__ == '__main__';
+
+if __name__ == "__main__";
     import MySQLdb
     from sys import argv
+
     # the arguments, username, password and database name and
-    # in order. 
+    # in order.
+
     username = argv[0]
     password = argv[1]
     database = argv[2]
+
     # connect to a MySQL server running on a localhost port 3306
+    
     db = MySQLdb.connect(host = 'localhost', port = 3306,
             user = username, pwd = password, db = database)
     cur = db.cursor()
+    
     """ select everything, from states database/file and ordered by
-    # state id in ascending order
-    # cursor executes the select, from and order by statements """
+    # state id in ascending order cursor executes the select, from
+    and order by statements """
+    
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    
     """ gets all rows from a cursor object """
+    
     rows = cur.fetchall()
+    
     """ iterates through and prints out all the rows, then close the
     cursor and database objects. """
     for row in rows:

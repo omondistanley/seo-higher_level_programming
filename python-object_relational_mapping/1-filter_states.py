e58f5030e63f440410ee0e1593e6373b8a093751
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-04 11:36:22 -0700

    Part one

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -14,14 +14,14 @@ if __name__ = "__main__";
     password = argv[2]
     database = argv[3]
 
-    """ Connect to a MySQL server running on a local host and
-        the given port. 
-    """
+        # Connect to a MySQL server running on a local host and
+        # the given port.
     db = MySQLdb.connect(host = "localhost", port = 3306; user =
             username, pin = password, db = database)
 
-    """
-        Create a cursor object connected to the database. 
-    """
+        # Create a cursor object connected to the database.
     cursor = db.cursor()
+
+        # Query that selects the states starting with letter N and
+        # has them ordered by state id. 
 

721c879c2381320dfa8e91c1cb2c75ab04cf46d1
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-05 16:32:08 -0700

    Part two"

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,3 +7,16 @@
 import MySQLdb
 from sys import argv
 
+if __name__ = "__main__":
+    # the arguments username, password, database and state name
+    # searched are taken in as command line arguments.
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    stateName = argv[4]
+
+    # connect the module to a local host at port 3306
+    db =MySQLdb.connect(host='localhost', port=3306, user=username
+                        password=password, database=database,
+                        stateName=stateName)
+

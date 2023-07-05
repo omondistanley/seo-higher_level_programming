74e09026fb4d17a029c64dbc1269a28482e06b32
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-05 10:42:35 -0700

    Part one

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,32 +1,40 @@
 #!/usr/bin/python3
 """
-    Script listing all the states starting with letter N
-    from the database. 
+    A script that lists all the states starting with
+    N in theie names and ordered by the state id in
+    ascending order.
 """
 
-from MySQLdb import mysql
-database = mysql
+import MySQLdb
+from sys import argv
 
-if __name__ = "__main__";
-    import argv
+if __name__ == "__main__":
+    # the arguments and they're being read in from the
+    # command line as command line arguments.
     username = argv[1]
     password = argv[2]
     database = argv[3]
 
-    database = msq.connect(host="localhost", port= 3306,
-            user="username", pwd="password", db ="database")
-    
+    # connecting the module to a local MySQL server running
+    # on port 3306. 
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=username, password=password,
+                         database=database)
+    # create a cursor object
     cursor = db.cursor()
-    query = SELECT * FROM states WHERE names LIKE 'N%'
-                ORDER BY states.id ASC
+    # execute a query that selects from the database states
+    # whose names start with N and returns in ascending order
+    query1 = "SELECT * FROM states WHERE names LIKE 'N%' "
+    query2 =  "ORDER BY states."
+    query = query1 + query2
     cursor.execute(query)
-
+    # fetch all rows for printing after the query.
     rows = cursor.fetchall()
-
+    # iterate through the rows while printing them out.
     for row in rows:
         print(row)
-    
-    cursor.close()
-    database.close()
 
-    
+    # close the cursor and db objects.
+    cursor.close()
+    db.close()
+

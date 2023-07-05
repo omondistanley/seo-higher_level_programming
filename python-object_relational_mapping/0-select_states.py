7742a6cfabb926dc6c6748f7df5a8ec267dd4e9f
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-05 09:18:16 -0700

    Part zero'

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
 """
-    
     A script that lists all states in ascending order
     according to the state.id
 """
 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
     # the arguments, username, password and database name
     # arguments are read from the command line args
-    username = sys.argv[0]
-    password = sys.argv[1]
-    database = sys.argv[2]
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
 
     # create a database object that connects to MySQL and a
     # localhost and port 3306.
 
-    db = MySQLdb.connect(host = 'localhost', port = 3306, user=
-            username, pwd = password, dAtaBAsE = database)
+    db = MySQLdb.connect(host='localhost', port=3306, user=
+            username, pwd=password, dAtaBAsE=database)
     # create a cursor object connected to the database.
     cursor = db.cursor()
     # executing a query with selecting from the states and is

35e28ca9cfd7f16b56d07176c72e61ff33f5da1d
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-05 10:19:08 -0700

    Part zero

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     # localhost and port 3306.
 
     db = MySQLdb.connect(host='localhost', port=3306,
-                         user=username, passwORd=password,
+                         user=username, password=password,
                          dAtaBAsE=database)
     # create a cursor object connected to the database.
     cursor = db.cursor()

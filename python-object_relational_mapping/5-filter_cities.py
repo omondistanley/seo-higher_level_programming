1f7a70a3bdac0b274201ff300ddb88720b5505c2
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-06 13:53:32 -0700

    Part five

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -36,9 +36,9 @@ if __name__ == "__main__":
     final = []
     # iterate through the results from the execution of the query,
     # adding them to the array.
-        for i in range(queryExecuted):
+    for i in range(queryExecuted):
         final.append(queryExecuted[i][0])
-    print(', '.join(final))
+        print(', '.join(final))
 
     # Close the cursor object.
     cursor.close()

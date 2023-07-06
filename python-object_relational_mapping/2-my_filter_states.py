08b85641675945b43afa2fb6cbb6c3b327b0e114
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-06 03:19:09 -0700

    part two

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-    A script that reeturns the states whose names are the same
-    as the one provided by the user.
+    A script that returns the states whose names
+    are the same as the one provided by the user.
+    The script results are returned in asc order.
 """
 
 import MySQLdb
@@ -10,7 +11,6 @@ from sys import argv
 if __name__ = "__main__":
     # the arguments username, password, database and state name
     # searched are taken in as command line arguments.
-    
     username = argv[1]
     password = argv[2]
     database = argv[3]
@@ -20,16 +20,17 @@ if __name__ = "__main__":
     db =MySQLdb.connect(host='localhost', port=3306, user=username
                         password=password, database=database,
                     stateName=stateName)
-
+    # create a cursor object for the database
     cursor = db.cursor()
     # query that executes to select from the database the states
     # whose name is the same as the state name searched by the user.
     cursor.execute("SELECT * FROM states WHERE name==stateName")
     # fetch all the rows after the query.
     rows = cursor.fetchall()
-
+    # iterate to print each row in the fetched rows after the query
     for row in rows:
         print(row)
-  
+
+    # close the cursor and db
     cursor.close()
     db.close()

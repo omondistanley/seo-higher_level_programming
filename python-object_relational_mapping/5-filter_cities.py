a94021360fdb438b923614d739c55f742ed7571d
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-06 11:24:34 -0700

    Part five

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -27,9 +27,8 @@ if __name__ == "__main__":
     # the states whose names match the argument provided and return
     # their cities.
     cursor.execute("SELECT cities.id, cities.name, states.name\
-                   FROM cities WHERE name = '{}'\
-                   JOIN states ON cities.state_id = states.id\
-                   ORDER BY cities.id ASC".format(stateName))
+                   FROM cities JOIN states ON cities.state_id = states.id\
+                   WHERE cities.name = '{}' ORDER BY cities.id ASC".format(stateName))
     # Fetch all rows after executing the query.
     rows = cursor.fetchall()
 

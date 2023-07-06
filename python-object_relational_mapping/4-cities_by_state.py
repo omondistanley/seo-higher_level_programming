ddea7384fb20c435f4ea0b2595934cb1448c4e1d
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-06 10:36:40 -0700

    Part four

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     # that returns the cities ordered in ascending order by
     # the id of the cities.
     cursor.execute("SELECT cities.id, cities.name, states.name\
-                   FROM cities JOIN states ON cities.states.id = states.id\
+                   FROM cities JOIN states ON cities.state_id = states.id\
                    ORDER BY cities.id ASC")
 
     # Fetch all rows after the query

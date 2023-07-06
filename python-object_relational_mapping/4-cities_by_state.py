51093155544b93aef25ac2cf63444a33d645a96f
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-06 10:14:41 -0700

    Part Four

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
 """
-    A script that lists all the cities from the states
-    list and database, the script should run on a local
-    host server running through port 3306.
+    A script that lists all the cities from the list and
+    the script should run on a local host server running
+    through port 3306.
 """
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    # The arguments are taken in from the command line 
-    # arguments.
+    # The arguments provided are taken in from the command
+    # line.
     username = argv[1]
     password = argv[2]
     database = argv[3]
 
-    # Connecting the script to the local hose server running
+    # Connecting the script to the local host server running
     # on port 3306.
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=username, password=password,
                          database=database)
-    
+
     # Create a cursor object for executing the queries.
     cursor = db.cursor()
-    # Usinng the cursor's execute function, execute a query
+    # Using the cursor's execute function, execute a query
     # that returns the cities ordered in ascending order by
     # the id of the cities.
     cursor.execute("SELECT * FROM cities ORDER BY cities.id ASC")
@@ -38,4 +38,3 @@ if __name__ == "__main__":
     # close the cursor object.
     cursor.close()
     db.close()
-

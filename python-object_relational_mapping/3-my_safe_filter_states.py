512b9a8eeafce0d5525a4ed384e499b3c6d4fbc4
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-06 09:06:25 -0700

    Part Three

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,40 +1,39 @@
 #!/usr/bin/python3
 """
-    A script that takes arguments from the user and displays
-    the state names that are the same as the user input. In
-    this case, let the script be SQL injection free.
+    A script that takes arguments and displays all the
+    states whose names match the argument provided, and
+    is immune to SQL injections.
 """
 
 import MySQLdb
 from sys import argv
 
 if __name__ = "__main__":
-    # The four arguments taken from the command line: username
-    # password, database and the state name searched
+    # The script takes four arguments from the command line
+    # as command line arguments.
     username = argv[1]
     password = argv[2]
     database = argv[3]
     stateName = argv[4]
 
-    # Connect the script to a MYSQL server running on a local
-    # host at port 3306.
-    db = MySQLdb.connect(host='localhost', port=3305, user=username,
-                         password=password, databse=databse)
-
-    # Create a cursor object that executes all the SQL queries for
-    # the data base after connection to the local host.
+    # Connect the module to a local host server running at
+    # port 3306.
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=username, password=password,
+                         database=database)
+    # Create a cursor object that executes the SQL queries
     cursor = db.cursor()
-    # Use the cursor to  execute an SQL query that selects from the
-    # states those whose names are the same as the argument providaed.
-    # The query should be SQL injection free.
-    cursor.execute("SELECT * FROM states WHERE BINARY name = '@%s'"
-                   "ORDER BY states.id ASC")
-    # fetch all the rows after the query
+    # Using the cursor's execute function, execute a query
+    # that selects from the states whose names are same as
+    # the one provided.
+    cursor.execute("SELECT * FROM states WHERE BINARY name = '@%S'"
+                   "ORDER BY states.id".format(stateName))
+    # fetch all the rows for printing after the query.
     rows = cursor.fetchall()
-    # iterate through the rows and print out the output.
+    # iterate through the rows and print the outcome.
     for row in rows:
         print(row)
 
-    # Close the cursor object.
+    # close the cursor object.
     cursor.close()
     db.close()

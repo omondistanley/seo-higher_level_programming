d1834e8d260641613d0a5a7c77c15c6ec51960c7
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-06 14:46:43 -0700

    model state

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    A class that contains the definition of a state and an
+    instance base. The class inherits from Base, links to
+    MySQL table, the class has an id attribute that represents
+    a column of an auto generated unique integer that is the
+    primary key. The class also has a name attribute representing
+    a column of a string with max of 128 characters.
+    Both attributes aren't null.
+"""
+
+

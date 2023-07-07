9368e332fe2ee6155b00ed546d44c4fb5704611a
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-07 05:21:21 -0700

    Part Seven

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,6 +6,7 @@
 """
 
 
+import SQLAlchemy
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
@@ -14,13 +15,27 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     # the arguments are taken in from the command line
     # Connect the script to the localhost at port.
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2], argv[3]))
+    db = SQLAlchemy.connect(host='localhost', port=3306,
+                            user=argv[1],password=argv[2],
+                            database=argv[3])
+
+    cursor = db.connect()
+    
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
+    # engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+    #                       .format(argv[1], argv[2], argv[3]))
     # Create a session class that is bound to the engine obj
-    Session = sessionmaker(bind=engine)
+    # Session = sessionmaker(bind=engine)
     # Create an instance of the the Session class that quaries all
     # inherited state objects and orders them by state id.
-    session = Session()
+    # session = Session()
     # iterate through the results after query and print the state name&id.
-    for state in session.query(State).order_by(State.id):
-        print('{}: {}'.format(state.id, state.name))
+    # for state in session.query(State).order_by(State.id):
+    #   print('{}: {}'.format(state.id, state.name))

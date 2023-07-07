42266f428e3bbcb486b33a32d36e1744f038fffa
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-06 19:21:45 -0700

    model_state

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -15,9 +15,9 @@ Base = declarative_base()
 class State(Base):
     # the class should be mapped to a table named states in the database
     __table__ = 'states'
-        # the id attribute representing a column set to a non-null integer
-        # and is the primary key(primary key set to true)
-        id = Column(Integer, primary_key = True)
-        # The class attribute with a string which holds a maximum of 128
-        # characters and can't be null(nullable value set to false.)
-        name = Column(String(128), nullable = False)
+    # the id attribute representing a column set to a non-null integer
+    # and is the primary key(primary key set to true)
+    id = Column(Integer, primary_key = True)
+    # The class attribute with a string which holds a maximum of 128
+    # characters and can't be null(nullable value set to false.)
+    name = Column(String(128), nullable = False)

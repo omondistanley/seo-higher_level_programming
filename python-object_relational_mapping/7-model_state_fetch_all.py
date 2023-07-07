cfa683cc81daee8c2f332af27ff92340600090c4
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-07 05:11:34 -0700

    Part Seven

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,25 +7,20 @@
 
 
 from sys import argv
-from model_state import State, Base
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     # the arguments are taken in from the command line
-    username = argv[1]
-    password = argv[2]
-    database = argv[3]
-    # Connect the script to the localhost at port and has a parameter
+    # Connect the script to the localhost at port.
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(argv[1], argv[2], argv[3]))
     # Create a session class that is bound to the engine obj
     Session = sessionmaker(bind=engine)
     # Create an instance of the the Session class that quaries all
     # inherited state objects and orders them by state id.
-    thisSession = Session()
+    session = Session()
     # iterate through the results after query and print the state name&id.
-    for state in thisSession.query(State).order_by(State.id):
+    for state in session.query(State).order_by(State.id):
         print('{}: {}'.format(state.id, state.name))
-
-    thisSession.commit()

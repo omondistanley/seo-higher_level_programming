0d7599a8bc73d6b117e0cdbca8a5eca8bc16f4ba
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-07 08:59:09 -0700

    Part eleven

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -11,9 +11,24 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     """
         Script takes in 3 arguments, inserts a new state object and
         prints the new states.id after creation.
     """
-
+    # Connect the script to a localhost port using an engine obj.
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    # Create a session class that is bound to the engine obj.
+    Session = sessionmaker(bind=engine)
+    # Create a new instance of the session class that is used for
+    # adding the new state.
+    session = Session()
+    # the state to be added,
+    added_state = 'Louisiana'
+    # add the new state to the session.
+    session.add(added_state)
+    # Commit the change to the session before proceeding
+    session.commit()
+    # Print out the id of the added state.
+    print(added_state.id)

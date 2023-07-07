75822fe63ba3c356975e2bb135c483cb9ca2932e
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-07 05:25:30 -0700

    Part Seven

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -23,4 +23,4 @@ if __name__ == "__main__":
     session = Session()
     # iterate through the results after query and print the state name&id.
     for state in session.query(State).order_by(State.id):
-       print('{}: {}'.format(state.id, state.name))
+        print('{}: {}'.format(state.id, state.name))

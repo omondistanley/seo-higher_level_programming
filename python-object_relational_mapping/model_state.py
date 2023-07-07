70bba81b1ed36918d17ccdc0575cc6652be7faec
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-06 19:38:14 -0700

    model state

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -10,6 +10,8 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+
 class State(Base):
     """
         the class should be mapped to a table named states in the database

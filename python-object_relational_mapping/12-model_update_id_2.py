28f95e5525b16ebdadce67036591fe75bcbdfcf6
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-07 10:14:43 -0700

    Part twelve

--- python-object_relational_mapping/12-model_update_id_2.py
+++ python-object_relational_mapping/12-model_update_id_2.py
@@ -10,4 +10,20 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+if __name__ == "__main__":
+    # Script takes in 3 arguments and connect the script to a localhost
+    # using engine obj
+    engine = create_engine('mysql+mysqldb://{}:{}localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    # create a session class bound to the engine object.
+    Session = sessionmaker(bind=engine)
+    # create an instance of the session class used to change the state
+    # object at id=2
+    session = Session()
+    # Using the instance session, query for the object at second id
+    states = session.query(State).filter(id=2).first()
+    # create the new state variable.
+    states.name = 'New Mexico'
+    # commit the changes to the database.
+    session.commit()
 

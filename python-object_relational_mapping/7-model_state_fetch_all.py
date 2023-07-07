857e88d0ced3f7a03442937c26dd9e34279b26c6
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-06 20:02:49 -0700

    Part seven

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,3 +1,12 @@
 #!/user/bin/python3
 """
-    A script that lists all the state objects. 
+    A script that lists all the state objects from the database.
+    The script should take three arguments, uses SQLAlchemy and
+    must import state and base.
+"""
+
+import SQLAlchemy
+from sys import argv
+
+if __name__ == "__main__":
+

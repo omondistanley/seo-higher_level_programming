d2aa10b87f5063b333aaff0dd4b2244d5ce1f3e1
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-06 19:24:53 -0700

    model_state

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,8 +5,7 @@
     two attributes, id and class.
 """
 
-# from the sqlalchemy, import Column, integer, string and declarative
-# base.
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -14,7 +13,7 @@ Base = declarative_base()
 
 class State(Base):
     # the class should be mapped to a table named states in the database
-    __table__ = 'states'
+    __tablename__ = 'states'
     # the id attribute representing a column set to a non-null integer
     # and is the primary key(primary key set to true)
     id = Column(Integer, primary_key = True)

f513446d56d6499a6f921ccbc33d0169ba8dbc22
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-11 14:55:13 -0700

    Deleting an item at a given index

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    deleting an item at a given index in a list.
+"""
+
+
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    else:
+        return my_list.remove(my_list[idx])

22d3150457491d7bfc8df067ed734b503ec8457a
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-11 15:35:12 -0700

    Part four

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -12,22 +12,18 @@ class Square:
 
     def __init__(self, size=0):
         self.size = size
-
     def area(self):
-        """ Method that gives the area of the square """
         area = self.size * self.size
         return area
-
     # @property to retrive the size attribute.
     def size(self):
         return self.__size
 
     # @property to set the size attribute to a given value.
     def size(self, value):
-        if type(value) == int and value >= 0:
-            self.__size = value
         """ Size must be an integer and greater than zero. """
         if type(value) != int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value

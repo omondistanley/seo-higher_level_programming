28cfcc14ce15d12665506149b8ed66b655083857
dimmed82 <omondistanley.oduor@gmail.com>
2023-07-11 16:21:47 -0700

    Part six

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -6,21 +6,19 @@
 
 class Square:
     """ The square class with size, position and area. """
+
     def __init__(self, size=0, position=(0, 0)):
-        # Initialize and make the size and position private
+    """ Initialize and make the size and position private """
         self._size = size
         self._position = position
 
     def area(self):
-        # Calculates and returms the area of the square.
         return self._size * self._size
     
     def size(self):
-        # gets the size property of the square.
         return self._size
 
     def size(self, value=0):
-        # sets the size to a given integer greater than 0
         if type(value) == int:
             if value < 0:
                 raise ValueError("size must be => 0")
@@ -30,12 +28,9 @@ class Square:
             raise TypeError("size must be an integer")
 
     def position(self):
-        # gets the position property of the square.
         return self._positon
 
     def position(self, value):
-        # sets the position property. Check if value is a
-        # tuple of ints >=0.
         if len(value) != 2 or not isinstance(value, tuple) or\
                 not all(isinstance(val, int) or\
                 not all(val >= 0 for val in value):
@@ -45,11 +40,10 @@ class Square:
         self._position = value
 
     def my_print(self):
-        # method that prints out the square details.
         if self._size == 0:
             print()
         else:
             for i in range(slef._position[1]):
                 print()
             for i in range(self._size):
-                print(" " * self._position[0] + '#' *self._size)
+                print(" " * self._position[0] + '#' * self._size)

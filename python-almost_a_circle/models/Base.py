2010b05c59e258228382c8e18fd1e49f09cddc7c
dimmed82 <omondistanley.oduor@gmail.com>
2023-08-20 18:34:30 -0700

    Part one - base.py

---
+++ python-almost_a_circle/models/Base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    A base class with a private attribute and a constructor.
+"""
+
+import json
+
+class Base(Object):
+
+    """
+        Private attribute _nb_objects = 0
+    """
+    __nb_objects = 0
+
+    """
+        Constructor initializingg the id variables etc. The id is a 
+        none-null integer.
+    """
+    def __init__(self, id=none):
+
+

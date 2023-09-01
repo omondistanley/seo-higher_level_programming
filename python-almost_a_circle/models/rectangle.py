9801470e6c1c6bc5b08c32176d1c6ea5b54e5d7e
dimmed82 <omondistanley.oduor@gmail.com>
2023-08-31 20:23:52 -0700

    Part two

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -5,5 +5,5 @@ Base = __import__('base').Base
 
 
 class Rectangle(Base):
-    def __init__(self, width, height, x-0, y-0, id=None):
+    def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)

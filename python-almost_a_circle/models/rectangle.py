f2322a9e43a0702d7c7d95b23f6edd43c4ea7d44
dimmed82 <omondistanley.oduor@gmail.com>
2023-09-01 07:40:06 -0700

    Part two - Rectangle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,5 +4,40 @@
 from models.base import Base
 
 class Rectangle(Base):
+    """ Initialization of the variables and calling the id
+        attribute from the Base suoer class. """
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    """ The getter property for the width """
+    def width(self):
+        return self.__width
+    """ The setter property for the width """
+    def width(self, val):
+        self.__width = val
+        return self.__width
+    """ The getter property for the height """
+    def height(self):
+        return self.__height
+    """ The setter property for the height """
+    def height(self, val)
+        self.__height = val
+        return self.__height
+    """ The getter property for x """
+    def x(self):
+        return self.__X
+    """ The setter property for x """
+    def x(self, val):
+        self.__x = val
+        return self.__x
+    """ The getter propety for y """
+    def y(self):
+        return self.__y
+    """ The setter property for y """
+    def y(self, val):
+        self.__y = val
+        retrun self.__y

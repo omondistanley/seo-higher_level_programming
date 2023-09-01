8f6acccbbb21d2f7de7515e9a5ea55aad83659ef
dimmed82 <omondistanley.oduor@gmail.com>
2023-08-31 20:09:32 -0700

    Part two Rectangle class

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" A rectangle class that inherits from the base class. """
+
+Base = __import__('base').Base
+
+
+class Rectangle(Base):
+""" Private instance varibles with their getters and setters. """
+
+    def __init__(self, width, height, x-0, y-0, id=None):
+        super().__init__(id)

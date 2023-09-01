fb02d3b80bc4d7c1a4f2156e9ea6801425dfb028
dimmed82 <omondistanley.oduor@gmail.com>
2023-08-31 19:52:53 -0700

    Part one - Base class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -16,12 +16,13 @@ class Base():
     __nb_objects = 0
 
     """
-        Constructor initializingg the id variables etc. The id is a 
-        none-null integer.
+        Constructor initializingg the id variables etc. The id is a
+        non-null integer.
     """
     def __init__(self, id=None):
-        """ Check if the id attribute is not null and set it to be the id attribute! """
-        """ If not increase __nb_objects and set it to the id variable.(private) """
+        """ Check if the id attribute is not null and set it
+            to be the id attribute! If not increase __nb_objects
+            and set it to the id variable.(private) """
         if id is not None:
             self.id = id
         else:

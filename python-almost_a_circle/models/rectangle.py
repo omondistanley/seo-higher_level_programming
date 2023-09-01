fa8f4cec6c8c62a9699812d0238a2ec584dd8945
dimmed82 <omondistanley.oduor@gmail.com>
2023-08-31 20:11:50 -0700

    Part two Rectangle class

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,5 @@ Base = __import__('base').Base
 
 
 class Rectangle(Base):
-""" Private instance varibles with their getters and setters. """
-
     def __init__(self, width, height, x-0, y-0, id=None):
         super().__init__(id)
